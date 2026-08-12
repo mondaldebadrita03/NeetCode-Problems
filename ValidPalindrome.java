542f36cf7383b3e7eba8d84f0759d2b4f28e8da6
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-13 01:01:07 +0530

    Update ValidPalindrome.java

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,3 +1,4 @@
+//My First Approach - TC: O(n) SC: O(n)
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
@@ -6,5 +7,20 @@ class Solution {
             reverse += s.charAt(i);
         }
         return s.equals(reverse);
+    }
+}
+
+//Optimal Approach - TC: O(n) SC: O(1)
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 }
